a8e3ae8e9c2d615be343dbf104380f79470c8b91
Vivian Fang <vivian98.fang@gmail.com>
2015-10-25 00:06:12 -0700

    Troubleshooting (1)

--- findme/apps/student/views.py
+++ findme/apps/student/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 from .models import *
 # Create your views here.
 def home(request):
-    render(request, 'home.html')
+    render(request, 'home.html', {})
--- findme/urls.py
+++ findme/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from apps.student import views
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'findme.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.home, name='home'),
-)
+    url(r'^$', apps.student.views.home, name='home'),
+]
